5a33e379a7d30bcff04e3eed70fec2ef9b2ddcc5
Pedro Daguer <pepedavila1308@gmail.com>
2024-11-15 17:29:19 +0000

    Implementação do tratamento de exceções e configuração para deploy no Railway.

--- BaseAPI/src/main/java/br/daguer/BaseAPI/BaseApiApplication.java
+++ BaseAPI/src/main/java/br/daguer/BaseAPI/BaseApiApplication.java
@@ -3,6 +3,7 @@ package br.daguer.BaseAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BaseApiApplication {
 
